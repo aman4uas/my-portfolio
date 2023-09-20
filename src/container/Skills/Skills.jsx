4d32317225d2af95d8c3ff43005c91d522786327
aman4uas <aman4u.as@gmail.com>
2023-09-20 18:32:30 +0530

    Update

--- src/container/Skills/Skills.jsx
+++ src/container/Skills/Skills.jsx
@@ -11,13 +11,23 @@ const Skills = () => {
 
   return (
     <>
-      <h2 className="head-text">Skills, Achievements & Experiences</h2>
+      <h2 className="head-text">
+        Skills & Career <span>Highlights</span>
+      </h2>
 
       <div className="app__skills-container">
         <motion.div className="app__skills-list">
           {skills.map((skill) => (
-            <motion.div whileInView={{ opacity: [0, 1] }} transition={{ duration: 0.5 }} className="app__skills-item app__flex" key={skill.name} >
-              <div className="app__flex" style={{ backgroundColor: skill.bgColor }}>
+            <motion.div
+              whileInView={{ opacity: [0, 1] }}
+              transition={{ duration: 0.5 }}
+              className="app__skills-item app__flex"
+              key={skill.name}
+            >
+              <div
+                className="app__flex"
+                style={{ backgroundColor: skill.bgColor }}
+              >
                 <img src={skill.icon} alt={skill.name} />
               </div>
               <p className="p-text">{skill.name}</p>
@@ -25,21 +35,32 @@ const Skills = () => {
           ))}
         </motion.div>
 
-        
         <div className="app__skills-exp">
           {experiences.map((experience) => (
-            <motion.div className="app__skills-exp-item" key={experience.year} >
+            <motion.div className="app__skills-exp-item" key={experience.year}>
               <div className="app__skills-exp-year">
                 <p className="bold-text">{experience.year}</p>
               </div>
               <motion.div className="app__skills-exp-works">
                 {experience.works.map((work) => (
                   <>
-                    <motion.div data-tip="" data-for={work.id} key={work.id} whileInView={{ opacity: [0, 1] }} transition={{ duration: 0.5 }} className="app__skills-exp-work">
+                    <motion.div
+                      data-tip=""
+                      data-for={work.id}
+                      key={work.id}
+                      whileInView={{ opacity: [0, 1] }}
+                      transition={{ duration: 0.5 }}
+                      className="app__skills-exp-work"
+                    >
                       <h4 className="bold-text">{work.name}</h4>
                       <p className="p-text">{work.company}</p>
                     </motion.div>
-                    <ReactTooltip id={work.id} effect="solid" arrowColor="#fff" className="skills-tooltip" >
+                    <ReactTooltip
+                      id={work.id}
+                      effect="solid"
+                      arrowColor="#fff"
+                      className="skills-tooltip"
+                    >
                       {work.desc}
                     </ReactTooltip>
                   </>
@@ -48,7 +69,6 @@ const Skills = () => {
             </motion.div>
           ))}
         </div>
-        
       </div>
     </>
   );
